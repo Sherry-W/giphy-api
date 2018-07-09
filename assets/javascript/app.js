f3f00aaba45baaa715826af8d8122086592a4426
Sherry-W <sherry.wu1219@gmail.com>
2018-07-08 22:16:34 -0700

    working without rating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,88 @@
 // alert('hi');
 
-var topics = ["rabbit", "hamster", "kitten", "puppy"];
+var topics = ["minions", "moana", "incredibles", "frozen", "zootopia", "inside out", "finding dory", "finding nemo", "big hero 6", "tangled", "monsters, inc.", "lion king", "mulan", "toy story", "lilo & stitch", "tarzan", "aladdin", "little mermaid", "cinderella", "disney peter pan", "pocahontas", "beauty and the beast", "sleeping beauty", "pinocchio", "bambi", "disney snow white"];
 var buttons;
 var userInput;
 
-
-for (var i = 0; i < topics.length; i++) {
-	// topics[i]
-	buttons = $('<button>').attr('class', 'click');
-	buttons.text(topics[i]);
-	$('#animalButton').append(buttons);
-}
-
-$(document).on('click', '#submitButton', function() {
-	event.preventDefault();
-	if ($('#animalInput').val() !== "") {
-		userInput = $('#animalInput').val();
-
-	    topics.push(userInput);
-	    buttons = $('<button>').attr('class', 'click');
-		buttons.text(userInput);
-		$('#animalButton').append(buttons);
-		$('#animalInput').val("");
-
-		console.log(userInput);
-		console.log(topics);
+// create array buttons
+	for (var i = 0; i < topics.length; i++) {
+		// topics[i]
+		buttons = $('<button>').attr('class', 'click');
+		buttons.text(topics[i]);
+		$('#disneyButton').append(buttons);
 	}
-});
+
+// add new buttons and push to array
+	$(document).on('click', '#submitButton', function() {
+		event.preventDefault();
+		if ($('#disneyInput').val() !== "") {
+			userInput = $('#disneyInput').val();
+
+		    topics.push(userInput);
+		    buttons = $('<button>').attr('class', 'click');
+			buttons.text(userInput);
+			$('#disneyButton').append(buttons);
+			$('#disneyInput').val("");
+
+			console.log(userInput);
+			console.log(topics);
+		}
+	});
+
+// clicks = 10 times (10 pictures at a time)
+	for (var j = 0; j < 10; j++) {
+
+// get giphys and display them
+	$(document).on('click', '.click', function() {
+
+	  	event.preventDefault();
+	    console.log($(this).text());
+	    $('#disneyImg').text("");
+
+	    var name = $(this).text();
+	    // var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&limit=10&tag=" + name + "";
+
+	    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg-13&tag=" + name + "";
+
+	    // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=dc6zaTOxFJmzC&rating=pg-13&limit=100&offset=10";
+
+	    // with rating
+	     // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + name + "&limit=100&api_key=dc6zaTOxFJmzC";
 
 
-var buttonTen = document.querySelector('.click');
-  	buttonTen.addEventListener('click', function () {
-for (var j = 0; j < 10; j++) {
 
-	//loop through the buttons and add the addEventListener to all the button
+	  	$.ajax({
+	  		url: queryURL,
+	  		method: "GET"
+	  	}).then(function(response){
+	  		console.log(response);
 
-  	// buttonTen.addEventListener('click', function () {
-// $(document).on('click', '.click', function() {
-  	event.preventDefault();
-    console.log($(this).text());
+// for (var k = 0; k < 10; k++) {
+// 	  		console.log(response.data[k].images.original.url);
+// 	  		console.log(response.data[k].images.original_still.url);
 
-    var name = $(this).text();
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&limit=10&tag=" + name + "";
+// 	  		var img = $('<img>').attr({"still":response.data[k].images.original_still.url, "anime":response.data[k].images.original.url, "data-type":"still", "src":response.data[k].images.original_still.url, "alt":name + " images"});
+// 	  		$('#disneyImg').prepend(img);
+// }
 
-  	$.ajax({
-  		url: queryURL,
-  		method: "GET"
-  	}).then(function(response){
-  		console.log(response);
-  		console.log(response.data.images.original.url);
-  		console.log(response.data.images.original_still.url);
+			console.log(response.data.images.original.url);
+	  		console.log(response.data.images.original_still.url);
 
-  		var img = $('<img>').attr({"still":response.data.images.original_still.url, "anime":response.data.images.original.url, "data-type":"still", "src":response.data.images.original_still.url, "alt":name + " images"});
-  		$('#animalImg').prepend(img);
+	  		var img = $('<img>').attr({"still":response.data.images.original_still.url, "anime":response.data.images.original.url, "data-type":"still", "src":response.data.images.original_still.url, "alt":name + " images"});
+	  		$('#disneyImg').prepend(img);
 
-  	});
-  // });
-}
-  });
+	  	});
 
-$(document).on('click', 'img', function(){
-	if ($(this).attr("data-type") == "still") {
-		$(this).attr("src", $(this).attr("anime"));
-		$(this).attr("data-type", "anime");
-	}else {
-		$(this).attr("src", $(this).attr("still"));
-		$(this).attr("data-type", "still");
+	  });
 	}
-});
+
+// anime & still giphys
+	$(document).on('click', 'img', function(){
+		if ($(this).attr("data-type") == "still") {
+			$(this).attr("src", $(this).attr("anime"));
+			$(this).attr("data-type", "anime");
+		}else {
+			$(this).attr("src", $(this).attr("still"));
+			$(this).attr("data-type", "still");
+		}
+	});
